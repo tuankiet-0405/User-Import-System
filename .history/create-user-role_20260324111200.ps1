87278baf54c980adc1131a9d99a55aaafc0a2126
Phan Lam Tuan Kiet <121711741+tuankiet-0405@users.noreply.github.com>
2026-03-24 11:18:09 +0700

    Delete .history directory

--- .history/create-user-role_20260324111200.ps1
+++
@@ -1,8 +0,0 @@
-$uri = "http://localhost:3000/api/v1/roles"
-$body = @{
-    name = "USER"
-    description = "Regular user role"
-} | ConvertTo-Json
-
-$response = Invoke-WebRequest -Uri $uri -Method POST -ContentType "application/json" -Body $body
-Write-Host $response.Content
